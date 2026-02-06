73ccd0a203540e04867165bdc9416f2381762d83
Nour <nourelden.0523066@gmail.com>
2026-02-06 15:30:02 +0200

    (ApiResponse): Create a Base Api Response Wrapper

---
+++ Manzili.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿using Manzili.Api.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manzili.Api.Controllers
+{
+    [ApiController]
+    public abstract class BaseApiController : ControllerBase
+    {
+        protected ActionResult<ApiResponse<T>> OkResponse<T>(T? data, string? message = null)
+        => Ok(ApiResponse<T>.Ok(data, message));
+
+        protected ActionResult<ApiResponse<T>> FailResponse<T>(string message, int statusCode)
+            => StatusCode(statusCode, ApiResponse<T>.Fail(message));
+    }
+}
