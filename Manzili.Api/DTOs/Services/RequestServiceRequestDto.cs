61561472e64a1e4b6c4b44405c70e5d129503d8c
Nour <nourelden.0523066@gmail.com>
2026-03-02 04:37:26 +0200

    (Services): create the RequestServiceDto

---
+++ Manzili.Api/DTOs/Services/RequestServiceRequestDto.cs
@@ -0,0 +1,26 @@
+﻿namespace Manzili.Api.DTOs.Services
+{
+    public class RequestServiceRequestDto
+    {
+        public int Id { get; set; }
+        public int BuyerId { get; set; }
+        public string? CustomizationText { get; set; }
+        public string? CustomRequestImage { get; set; }
+        public int Quantity { get; set; } = 1;
+
+        public List<OptionsGroup> Options { get; set; } = new List<OptionsGroup>();
+    }
+
+    public class OptionsGroup
+    {
+        public int Id { get; set; }
+        public List<OptionItem> OptionItems { get; set; } = new List<OptionItem>();
+    }
+
+    public class OptionItem
+    {
+        public int Id { get; set; }
+        public decimal? PriceAdjustment { get; set; }
+        public int Quantity { get; set; } = 1;
+    }
+}
