459683d1a0572d1f859ee175516c7b96124a1a36
Nour <nourelden.0523066@gmail.com>
2026-03-04 05:49:49 +0200

    (Services): update RequestServiceDto V2

--- Manzili.Api/DTOs/Services/RequestServiceRequestDto.cs
+++ Manzili.Api/DTOs/Services/RequestServiceRequestDto.cs
@@ -1,26 +1,18 @@
 ﻿namespace Manzili.Api.DTOs.Services
 {
+
     public class RequestServiceRequestDto
     {
-        public int Id { get; set; }
-        public int BuyerId { get; set; }
+        public int ServiceId { get; set; }
         public string? CustomizationText { get; set; }
         public string? CustomRequestImage { get; set; }
         public int Quantity { get; set; } = 1;
-
-        public List<OptionsGroup> Options { get; set; } = new List<OptionsGroup>();
+        public List<SelectedOptionGroupDto> OptionGroups { get; set; } = new();
     }
 
-    public class OptionsGroup
+    public class SelectedOptionGroupDto
     {
-        public int Id { get; set; }
-        public List<OptionItem> OptionItems { get; set; } = new List<OptionItem>();
-    }
-
-    public class OptionItem
-    {
-        public int Id { get; set; }
-        public decimal? PriceAdjustment { get; set; }
-        public int Quantity { get; set; } = 1;
+        public int GroupId { get; set; }
+        public List<int> OptionIds { get; set; } = new();
     }
 }
