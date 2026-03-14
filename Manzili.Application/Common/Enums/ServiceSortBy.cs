f6a4ea9f8fc36706e6d96eacbb299bb57c0f9c7e
Nour <nourelden.0523066@gmail.com>
2026-03-14 19:03:25 +0200

    (Services): add Service filter types and sorts type Enums for scalability

---
+++ Manzili.Application/Common/Enums/ServiceSortBy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manzili.Application.Common.Enums
+{
+    public enum ServiceSortBy
+    {
+        Newest = 0,
+        PriceAsc = 1,
+        PriceDesc = 2,
+        MostPurchased = 3
+    }
+}
