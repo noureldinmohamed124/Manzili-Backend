a771268fc29ef17fc451594009a5fd4c47fdb1b7
Nour <nourelden.0523066@gmail.com>
2026-03-14 19:41:58 +0200

    Change Persistence folder name to 'Data'

---
+++ Manzili.Infrastructure/Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Manzili.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manzili.Infrastructure.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Slug)
+                .HasMaxLength(150)
+                .IsUnicode(false)
+                .IsRequired();
+
+            builder.HasIndex(x => x.Slug)
+                .IsUnique();
+
+            builder.Property(x => x.NameAr)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.IsActive)
+                .HasDefaultValue(true);
+
+            builder.Property(x => x.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.HasOne(x => x.ParentCategory)
+                .WithMany(x => x.ChildCategories)
+                .HasForeignKey(x => x.ParentCategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Property(c => c.ParentCategoryId)
+                .IsRequired(false);
+        }
+    }
+}
---
+++ Manzili.Infrastructure/Data/Configurations/ServiceOptionConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Manzili.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manzili.Infrastructure.Persistence.Configurations
+{
+    public class ServiceOptionConfiguration : IEntityTypeConfiguration<ServiceOption>
+    {
+        public void Configure(EntityTypeBuilder<ServiceOption> builder)
+        {
+            builder.ToTable("ServiceOptions");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.ServiceOptionName)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.PriceAdjustment)
+                .HasPrecision(18, 2)
+                .IsRequired(false);
+
+            builder.Property(x => x.DisplayOrder)
+                .HasDefaultValue(0);
+
+            builder.Property(x => x.CreatedAt)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.HasIndex(x => new { x.OptionGroupId, x.ServiceOptionName })
+                    .IsUnique()
+                    .HasDatabaseName("UX_ServiceOption_Group_Name");
+
+            builder.HasOne(x => x.OptionGroup)
+                .WithMany(g => g.Options)
+                .HasForeignKey(x => x.OptionGroupId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
