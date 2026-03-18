c032482d278ef53729fd3c099974066cbb911b14
Nour <nourelden.0523066@gmail.com>
2026-03-18 12:05:32 +0200

    (Helpers): add service helper to update fields without needing load the entity first before update

---
+++ Manzili.Infrastructure/Helpers/ServiceUpdateHelper.cs
@@ -0,0 +1,38 @@
+﻿using Manzili.Domain.Entities;
+using Manzili.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manzili.Infrastructure.Helpers
+{
+    public class ServiceUpdateHelper
+    {
+        private readonly ManziliDbContext _context;
+
+        public ServiceUpdateHelper(ManziliDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task UpdateAsync(int serviceId, Expression<Func<SetPropertyCalls<Service>, SetPropertyCalls<Service>>> set)
+        {
+            await _context.Services
+                .ExecuteUpdateAsync(set);
+
+        }
+
+        public async Task UpdateAsync(int serviceId, Expression<Func<Service, bool>> predicate, Expression<Func<SetPropertyCalls<Service>, SetPropertyCalls<Service>>> set)
+        {
+            await _context.Services
+                .Where(predicate)
+                .ExecuteUpdateAsync(set);
+        }
+
+    }
+}
